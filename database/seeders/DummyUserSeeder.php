f8f46de96c10ea0ed16059c134f887023e3112a5
mahezarya <mahezarya23@gmail.com>
2023-11-02 19:54:39 +0700

    UPDATE : Data dummy user untuk user dan admin

--- database/seeders/DummyUserSeeder.php
+++ database/seeders/DummyUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,24 @@ class DummyUserSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $user = 
+        [
+            [
+                'name' => 'user',
+                'email' => 'user@user.com',
+                'role' => 'user',
+                'password' => bcrypt('user123')
+            ],
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin123'),
+            ]
+        ];
+
+        foreach($user as $key => $value)
+        {
+            User::create($value);
+        }
     }
 }
